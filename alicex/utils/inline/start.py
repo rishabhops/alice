ae4704fb0eb99f50447a61f31b04d19375de5823
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-07-27 07:38:20 +0530

    Update start.py

--- alicex/utils/inline/start.py
+++ alicex/utils/inline/start.py
@@ -29,5 +29,6 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
