df119525b788472a3683ce9276923ce59fb1dacf
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2025-01-27 05:48:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def main():
   return "Bot is Working..."
 
-if name == "main":
+if __name__ == "__main__":
   app.run(host="0.0.0.0")
